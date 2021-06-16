f2780ec5553334b080f810b9931512f133b054d4
Benjie <benjiebtigley@gmail.com>
2021-06-16 16:10:39 +0800

    Updated Robofriends

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,7 +30,6 @@ function App () {
     (
         <div className='tc'>
             <h1 className='f1'>RoboFriends</h1>
-            <button onClick={()=>setCount(count+1)}>Click</button>
             <SearchBox searchChange={onSearchChange}/>
             <Scroll>
                 <ErrorBoundry>
